6aec78167a46a049780695796ee6137b82edebfc
petpusin <attachai.p@likemeasia.com>
2024-11-28 23:30:32 +0700

    test: 💍 log

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
+    console.log("🚀 ~ AppService ~ getHello ~ getHello:")
     return 'Hello World!';
   }
 }
